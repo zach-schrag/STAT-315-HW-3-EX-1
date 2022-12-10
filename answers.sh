87afe84ec1f493730f12f7b8d08410ac8115702d
nabigha-mogharbel <nabiga.ba.mo1998@gmail.com>
2022-12-10 12:42:52 +0200

    Creating Table

--- answers.sh
+++ answers.sh
@@ -5,3 +5,16 @@ SELECT Points FROM Students WHERE Name='Alex';
 INSERT INTO Students VALUES(10, 'Nabigha', '24', 'F', 500);
 UPDATE Students SET Points = 500 WHERE Name='Basma';
 UPDATE Students SET Points = 100 WHERE Name='Alex';
+
+CREATE TABLE graduates(
+    ID INTEGER  PRIMARY KEY AUTOINCREMENT,
+    Name  TEXT NOT NULL UNIQUE,
+    Age INTEGER,
+	Gender TEXT,
+	Points INTEGER,
+	Graduation TEXT
+);
+
+INSERT INTO graduates(Name, Age, Gender, Points) SELECT Name, Age, Gender, Points FROM Students WHERE Students.Name='Layal';
+UPDATE graduates SET Graduation='08-09-2018' WHERE Name='Layal';
+DELETE FROM graduates WHERE Name='Layal';
