66e8311793f9c0eb4efecafce1c0cd708666b2a3
nabigha-mogharbel <nabiga.ba.mo1998@gmail.com>
2022-12-10 15:03:35 +0200

    Count & Filter

--- answers.sh
+++ answers.sh
@@ -22,3 +22,9 @@ DELETE FROM graduates WHERE Name='Layal';
 SELECT employees.name, employees.Company, companies.date FROM employees Inner join companies ON employees.Company=companies.name;
 SELECT employees.name FROM employees Inner join companies ON employees.Company=companies.name and companies.Date < 2000;
 SELECT companies.name FROM companies Inner join employees ON employees.Company=companies.name and employees.Role='Graphic Designer';
+
+select name from students where points=(SELECT MAX(points) from students);
+select AVG(points) from students;
+select COUNT(Name) from students where points=500;
+select Name from students WHERE Name GLOB '*s*';
+select name from students ORDER BY points DESC;
