84b575127954ee6fbd5de3ec902dd9ec99dbaea9
nabigha-mogharbel <nabiga.ba.mo1998@gmail.com>
2022-12-10 14:46:21 +0200

    Joins

--- answers.sh
+++ answers.sh
@@ -18,3 +18,7 @@ CREATE TABLE graduates(
 INSERT INTO graduates(Name, Age, Gender, Points) SELECT Name, Age, Gender, Points FROM Students WHERE Students.Name='Layal';
 UPDATE graduates SET Graduation='08-09-2018' WHERE Name='Layal';
 DELETE FROM graduates WHERE Name='Layal';
+
+SELECT employees.name, employees.Company, companies.date FROM employees Inner join companies ON employees.Company=companies.name;
+SELECT employees.name FROM employees Inner join companies ON employees.Company=companies.name and companies.Date < 2000;
+SELECT companies.name FROM companies Inner join employees ON employees.Company=companies.name and employees.Role='Graphic Designer';
