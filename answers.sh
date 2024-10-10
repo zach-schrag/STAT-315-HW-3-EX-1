309f351cae6b7a2491f4b3ae5f153bc7240bfa09
Zach <zach.schrag@tamu.edu>
2024-10-09 23:19:37 -0500

    Creating Table

--- answers.sh
+++ answers.sh
@@ -13,3 +13,23 @@ VALUES ('Zach', '21', 'Male', 100);
 UPDATE students SET Points = Points + 10 WHERE Name = 'Basma';
 
 UPDATE students SET Points = Points - 5 WHERE Name = 'Alex';
+
+# Layla
+CREATE TABLE graduates (
+    ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    Name TEXT NOT NULL UNIQUE,
+    Age INTEGER,
+    Gender TEXT,
+    Points INTEGER,
+    Graduation TEXT
+);
+
+INSERT INTO graduates (Name, Age, Gender, Points)
+SELECT Name, Age, Gender, Points FROM students WHERE Name = 'Layal';
+
+UPDATE graduates SET Graduation = '08/09/2018' WHERE Name = 'Layal';
+
+DELETE FROM students WHERE Name = 'Layal';
+
+
+
