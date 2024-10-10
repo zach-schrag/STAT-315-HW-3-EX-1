2fdc5dbf2d619f718f011dee2199b0fb064438ab
Zach <zach.schrag@tamu.edu>
2024-10-09 23:01:01 -0500

    Basic Queries

--- answers.sh
+++ answers.sh
@@ -1,30 +1,15 @@
-SELECT Name FROM Students;
-SELECT * FROM Students WHERE Age > 30;
-SELECT Name FROM Students WHERE Gender='F' AND Age=30;
-SELECT Points FROM Students WHERE Name='Alex';
-INSERT INTO Students VALUES(10, 'Nabigha', '24', 'F', 500);
-UPDATE Students SET Points = 500 WHERE Name='Basma';
-UPDATE Students SET Points = 100 WHERE Name='Alex';
+# basic queries
+SELECT Name FROM students;
 
-CREATE TABLE graduates(
-    ID INTEGER  PRIMARY KEY AUTOINCREMENT,
-    Name  TEXT NOT NULL UNIQUE,
-    Age INTEGER,
-	Gender TEXT,
-	Points INTEGER,
-	Graduation TEXT
-);
+SELECT * FROM students WHERE Age > 30;
 
-INSERT INTO graduates(Name, Age, Gender, Points) SELECT Name, Age, Gender, Points FROM Students WHERE Students.Name='Layal';
-UPDATE graduates SET Graduation='08-09-2018' WHERE Name='Layal';
-DELETE FROM graduates WHERE Name='Layal';
+SELECT Name FROM students WHERE Gender = 'Female' AND Age = 30;
 
-SELECT employees.name, employees.Company, companies.date FROM employees Inner join companies ON employees.Company=companies.name;
-SELECT employees.name FROM employees Inner join companies ON employees.Company=companies.name and companies.Date < 2000;
-SELECT companies.name FROM companies Inner join employees ON employees.Company=companies.name and employees.Role='Graphic Designer';
+SELECT Points FROM students WHERE Name = 'Alex';
 
-select name from students where points=(SELECT MAX(points) from students);
-select AVG(points) from students;
-select COUNT(Name) from students where points=500;
-select Name from students WHERE Name GLOB '*s*';
-select name from students ORDER BY points DESC;
+INSERT INTO students (Name, Age, Gender, Points)
+VALUES ('Zach', '21', 'Male', 100);
+
+UPDATE students SET Points = Points + 10 WHERE Name = 'Basma';
+
+UPDATE students SET Points = Points - 5 WHERE Name = 'Alex';
